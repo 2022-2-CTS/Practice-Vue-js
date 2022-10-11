f40ab8845de46c3a2c692350e9fb6aa68a6ff9af
toughC <saysoom@naver.com>
2022-10-11 12:53:10 +0900

    update 2

---
+++ Soobin/vue-practice/src/components/InitMain.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    
+    <button class="button" v-on:click="likecnt++">khkh</button> 
+    <span>ÁÁ¾Æ¿ä : {{likecnt}}</span>
+    <button class="button" @click="dislikecnt++">½È¾î¿ä</button> 
+    <span>½È¾î¿ä : {{dislikecnt}}</span>
+  </div>
+</template>
+
+
+
+<script>
+  export default {
+    name: 'InitMain',
+    props: {
+      msg: String
+    }
+  }
+  </script>
+
+<style>
+  .button{
+    margin-left : 15px;
+  }
+</style>
+
+
